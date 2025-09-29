5133b69044281d8540d9fbe1975989e17c215328
crisog <ortega.cpp@gmail.com>
2025-09-29 12:04:14 -0400

    chore: adds node/mdx adapters

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import node from '@astrojs/node';
+
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  integrations: [mdx()]
 });
